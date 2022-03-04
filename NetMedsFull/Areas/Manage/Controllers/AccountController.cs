9f9a9dd71043279a3c5e56db587f2049d57ce4e9
Ibragimli <elnur204@gmail.com>
2022-03-04 19:51:09 +0400

    manage index and login html

---
+++ NetMedsFull/Areas/Manage/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetMedsFull.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetMedsFull.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
