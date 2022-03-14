160ca7294d689ea3549cf3499648385fdb99dd88
Ibragimli <elnur204@gmail.com>
2022-03-14 18:15:14 +0400

    head button fixed

--- NetMedsFull/wwwroot/js/searchFilter.js
+++ NetMedsFull/wwwroot/js/searchFilter.js
@@ -1,12 +1,11 @@
-﻿var $rows = $('#searchUl li');
-$rows.hide();
-$('#searchBoxInput').keyup(function () {
-    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+﻿//var $rows = $('#searchUl li');
+//$rows.hide();
+//$('#searchBoxInput').keyup(function () {
+//    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
 
-    $rows.show().filter(function () {
-        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-        return !~text.indexOf(val);
-    }).hide();
-});
-
+//    $rows.show().filter(function () {
+//        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+//        return !~text.indexOf(val < 6);
+//    }).hide();
+//});
 
